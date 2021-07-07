5305b5a9e1a858925343e3bec32ea63571cb2adb
Diego Ballesteros <diballesterosc@gmail.com>
2021-07-06 20:34:20 -0500

    Added animations for profile section

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,4 +1,11 @@
 import React, { useRef } from "react"
+import {
+  animated,
+  useChain,
+  useSpring,
+  useSpringRef,
+  useTransition,
+} from "react-spring"
 import picture from "../../../static/img/Profile.jpg"
 import Framework from "../../assets/svg/framework.svg"
 import Database from "../../assets/svg/database.svg"
@@ -9,57 +16,127 @@ import Typescript from "../../assets/svg/Typescript.svg"
 import useIntersectionObserver from "../../hooks/useIntersectionObserver"
 import "./AboutMe.scss"
 
+const SKILLS = [
+  {
+    svg: <Programming />,
+    h3: "Web Development",
+    p: "HTML, CSS, SASS",
+  },
+  {
+    svg: <Framework />,
+    h3: "Frameworks",
+    p: "React, React Native, AngularJS",
+  },
+  {
+    svg: (
+      <Typescript className="aboutme__inner__container__skills__skill__ts" />
+    ),
+    h3: "Languages",
+    p: "JavaScript, TypeScript",
+  },
+  {
+    svg: <Test />,
+    h3: "Testing",
+    p: "Cypress, React Testing Library",
+  },
+  {
+    svg: <Docker />,
+    h3: "DevOps",
+    p: "Docker, TFS",
+  },
+  {
+    svg: <Database />,
+    h3: "Web Databases",
+    p: "SQL Server, Oracle",
+  },
+]
+
 const AboutMe: React.FC = () => {
-  const aboutMeRef = useRef()
-  const data = useIntersectionObserver(aboutMeRef, {})
+  const profileRef = useRef()
+  const profileApi = useSpringRef()
+  const skillsApi = useSpringRef()
+  const headerRef = useRef()
+  const dividerApi = useSpringRef()
+  const headerApi = useSpringRef()
+  const data = useIntersectionObserver(profileRef, {})
+  const dataHeader = useIntersectionObserver(headerRef, {})
+
+  const headerStyle = useSpring({
+    config: { duration: 500 },
+    from: { opacity: 0, left: "-500px" },
+    to: {
+      opacity: dataHeader?.isIntersecting ? 1 : 0,
+      left: dataHeader?.isIntersecting ? "0px" : "-500px",
+    },
+    ref: headerApi,
+  })
+
+  const dividerStyle = useSpring({
+    config: { duration: 500 },
+    from: { opacity: 0, left: "-500px" },
+    to: {
+      opacity: dataHeader?.isIntersecting ? 1 : 0,
+      left: dataHeader?.isIntersecting ? "0px" : "-500px",
+    },
+    ref: dividerApi,
+  })
+
+  const profileStyle = useSpring({
+    config: { duration: 750 },
+    from: { opacity: 0, left: "-500px" },
+    to: {
+      opacity: data?.isIntersecting ? 1 : 0,
+      left: data?.isIntersecting ? "0px" : "-500px",
+    },
+    ref: profileApi,
+  })
+
+  const transition = useTransition(data?.isIntersecting ? SKILLS : [], {
+    config: { duration: 750 },
+    ref: skillsApi,
+    trail: 400 / SKILLS.length,
+    from: { opacity: 0, scale: 0 },
+    enter: { opacity: 1, scale: 1 },
+    leave: { opacity: 0, scale: 0 },
+  })
+
+  useChain([headerApi, dividerApi, profileApi, skillsApi], [0, 0.2, 0.2, 0])
 
   return (
     <section className="aboutme" id="home">
-      <div className="aboutme__inner page" ref={aboutMeRef}>
-        <div className="aboutme__inner__header">
-          <h2>ABOUT ME</h2>
-          <div className="aboutme__inner__header__underline" />
+      <div className="aboutme__inner page">
+        <div className="aboutme__inner__header" ref={headerRef}>
+          <animated.h2 style={headerStyle}>ABOUT ME</animated.h2>
+          <animated.div
+            className="aboutme__inner__header__underline"
+            style={dividerStyle}
+          />
         </div>
         <div className="aboutme__inner__container">
-          <div className="aboutme__inner__container__profile">
+          <animated.div
+            className="aboutme__inner__container__profile"
+            style={profileStyle}
+          >
             <img src={picture} alt="profile" />
             <h3>Who am I?</h3>
-            <p>
+            <p ref={profileRef}>
               I work at <a href="https://arandasoft.com/">Aranda Software</a> as
               a senior front-end web developer where I help deliver a
               large-scale help desk web application to clients throughout Latin
               America.
             </p>
-          </div>
+          </animated.div>
           <div className="aboutme__inner__container__skills">
-            <div className="aboutme__inner__container__skills__skill">
-              <Programming /> <h3>Web Development</h3>
-              <p>HTML, CSS, SASS</p>
-            </div>
-            <div className="aboutme__inner__container__skills__skill">
-              <Framework /> <h3>Frameworks</h3>
-              <p>React, React Native, AngularJS</p>
-            </div>
-            <div className="aboutme__inner__container__skills__skill">
-              <Typescript className="aboutme__inner__container__skills__skill__ts" />
-              <h3>Languages</h3>
-              <p>JavaScript, TypeScript</p>
-            </div>
-            <div className="aboutme__inner__container__skills__skill">
-              <Test />
-              <h3>Testing</h3>
-              <p>Cypress, React Testing Library</p>
-            </div>
-            <div className="aboutme__inner__container__skills__skill">
-              <Docker />
-              <h3>DevOps</h3>
-              <p>Docker, TFS</p>
-            </div>
-            <div className="aboutme__inner__container__skills__skill">
-              <Database />
-              <h3>Databases</h3>
-              <p>SQL Server, Oracle</p>
-            </div>
+            {transition((style, item) => (
+              <animated.div
+                className="aboutme__inner__container__skills__skill"
+                style={{ ...style }}
+              >
+                {item.svg}
+                <h3>{item.h3}</h3>
+                <p>{item.p}</p>
+              </animated.div>
+            ))}
           </div>
         </div>
       </div>
